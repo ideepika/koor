6ebf5b7c242ccecd9f1f2e7105de741ce8933b65
Deepika Upadhyay <deepika@koor.tech>
2023-05-25 13:28:04 +0530

    ci: test
    Signed-off-by: Deepika Upadhyay <deepika@koor.tech>

--- ee/scripts/rebase_rook_master.sh
+++ ee/scripts/rebase_rook_master.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
+set -x
+
+GITHUB_TOKEN=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
+
 if [[ -z "$GITHUB_TOKEN" ]]; then
 	echo "Set the GITHUB_TOKEN env variable."
 	exit 1
 fi
 
-
 ROOK_REMOTE="https://github.com/rook/rook.git"
 ROOK_BRANCH="master"
 INPUT_AUTOSQUASH=false
 
-git remote add fork "$ROOK_REMOTE"
-git fetch $ROOK_REMOTE $ROOK_BRANCH
+git remote add rook_fork "$ROOK_REMOTE"
+git fetch rook_fork $ROOK_BRANCH
 
 set -o xtrace
 
 # do the rebase
-git checkout -b fork/rebase-rook-master fork/rebase-rook-master
+git checkout -b rebase-rook-master
 if [[ $INPUT_AUTOSQUASH == 'true' ]]; then
-	GIT_SEQUENCE_EDITOR=: git rebase -i --autosquash $ROOK_REMOTE/$ROOK_BRANCH
+	GIT_SEQUENCE_EDITOR=: git rebase -i --autosquash rook_fork/$ROOK_BRANCH
 else
-	git rebase $ROOK_REMOTE/$ROOK_BRANCH
+	git rebase rook_fork/$ROOK_BRANCH
 fi
 
 # push back
-git push --force-with-lease fork fork/rebase-rook-master:rebase-rook-master
+git push --force-with-lease origin rebase-rook-master:rebase-rook-master
